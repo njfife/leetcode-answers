ab7aed58b927cbb3432ca0148f966fcda5525804
Nathan <njfife12@gmail.com>
2022-07-24 20:45:34 -0600

    solution for matrix reshape

--- src/main/kotlin/com/fife/leetcode/questions/ds/q566/MatrixReshape.kt
+++ src/main/kotlin/com/fife/leetcode/questions/ds/q566/MatrixReshape.kt
@@ -1,10 +1,38 @@
 package com.fife.leetcode.questions.ds.q566
 
 fun main() {
-    Solution().matrixReshape(arrayOf(), 1, 1)
+    val reshaped = Solution().matrixReshape(arrayOf(
+        intArrayOf(1,2),
+        intArrayOf(3,4)
+    ), 2, 4)
+
+    for (row in reshaped) {
+        println(row.contentToString())
+    }
+
 }
 class Solution {
     fun matrixReshape(mat: Array<IntArray>, r: Int, c: Int): Array<IntArray> {
-        return arrayOf()
+        if (mat.size * mat[0].size != r * c) {
+            return mat
+        }
+        val reshapedArray = Array(r) {
+            IntArray(c)
+        }
+        var newArrayRow = 0
+        var newArrayCol = 0
+        for (rowIndex in mat.indices) {
+            val row = mat[rowIndex]
+            for (colIndex in row.indices) {
+                if (newArrayCol > c - 1) {
+                    newArrayRow++
+                    newArrayCol = 0
+                }
+                reshapedArray[newArrayRow][newArrayCol] = row[colIndex]
+                newArrayCol++
+
+            }
+        }
+        return reshapedArray
     }
 }
