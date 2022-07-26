5a9c05bbdbcd3d4e004f3602e24ded474d4fc227
Nathan <njfife12@gmail.com>
2022-07-25 22:48:48 -0600

    Solution for pascals triangle

--- src/main/kotlin/com/fife/leetcode/questions/ds/q118/PascalsTriangle.kt
+++ src/main/kotlin/com/fife/leetcode/questions/ds/q118/PascalsTriangle.kt
@@ -1,11 +1,24 @@
 package com.fife.leetcode.questions.ds.q118
 
-
+fun main() {
+    val result = Solution().generate(7)
+    println(result)
+}
 class Solution {
     fun generate(numRows: Int): List<List<Int>> {
-        return listOf()
+        val triangle = mutableListOf<MutableList<Int>>()
+        repeat(numRows) { row ->
+            triangle.add(mutableListOf())
+            repeat(row + 1) {
+                if (it == 0 || it == row) {
+                    triangle[row].add(1)
+                } else {
+                    triangle[row].add(
+                        triangle[row - 1][it - 1] + triangle[row - 1][it]
+                    )
+                }
+            }
+        }
+        return triangle
     }
-}
-
-class PascalsTriangle {
 }
