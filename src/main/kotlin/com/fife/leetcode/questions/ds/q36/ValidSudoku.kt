77d027731086e6e4d16b5ec5a931469ccba1c9f5
Nathan <njfife12@gmail.com>
2022-07-25 23:09:25 -0600

    Setup sudoku board

---
+++ src/main/kotlin/com/fife/leetcode/questions/ds/q36/ValidSudoku.kt
@@ -0,0 +1,23 @@
+package com.fife.leetcode.questions.ds.q36
+
+fun main() {
+    val solution = Solution().isValidSudoku(arrayOf(
+        charArrayOf('5','3','.','.','7','.','.','.','.'),
+        charArrayOf('6','.','.','1','9','5','.','.','.'),
+        charArrayOf('.','9','8','.','.','.','.','6','.'),
+        charArrayOf('8','.','.','.','6','.','.','.','3'),
+        charArrayOf('4','.','.','8','.','3','.','.','1'),
+        charArrayOf('7','.','.','.','2','.','.','.','6'),
+        charArrayOf('.','6','.','.','.','.','2','8','.'),
+        charArrayOf('.','.','.','4','1','9','.','.','5'),
+        charArrayOf('.','.','.','.','8','.','.','7','9')
+    ))
+
+    println("Board valid: $solution")
+}
+
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        return true
+    }
+}
