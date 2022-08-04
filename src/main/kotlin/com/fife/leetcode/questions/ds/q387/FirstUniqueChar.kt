e63aa0bef48231e4efef1a2b1b8661918ec9e1be
Nathan <njfife12@gmail.com>
2022-08-03 22:24:45 -0600

    First Unique Char in a string solution

--- src/main/kotlin/com/fife/leetcode/questions/ds/q387/FirstUniqueChar.kt
+++ src/main/kotlin/com/fife/leetcode/questions/ds/q387/FirstUniqueChar.kt
@@ -2,13 +2,30 @@ package com.fife.leetcode.questions.ds.q387
 
 fun main() {
 
-    val solution = Solution().firstUniqChar("This is my hot string")
+    val solution = Solution().firstUniqChar("this is my hot string")
 
     println("Index of first unique char: $solution")
 }
 
 class Solution {
     fun firstUniqChar(s: String): Int {
-        return 8
+        val chars = mutableMapOf<Char, Int>()
+        var firstUnique: Int = -1
+        for (strIndex in s.indices) {
+            if(chars[s[strIndex]] == -1) {
+                continue
+            }
+            if(chars[s[strIndex]] == null) {
+                chars[s[strIndex]] = strIndex
+            } else {
+                chars[s[strIndex]] = -1
+            }
+        }
+        for (index in chars) {
+            if ((firstUnique == -1 && index.value != -1) || index.value < firstUnique && index.value != -1) {
+                firstUnique = index.value
+            }
+        }
+        return -1
     }
 }
