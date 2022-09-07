b9b2e6dca5f6f7ed538bc674d90ed334a0f50b64
Nathan <njfife12@gmail.com>
2022-09-06 18:41:07 -0600

    Remove merged arrays solution to try again

--- src/main/kotlin/com/fife/leetcode/questions/ds/q88/MergeSortedArray.kt
+++ src/main/kotlin/com/fife/leetcode/questions/ds/q88/MergeSortedArray.kt
@@ -8,21 +8,6 @@ fun main() {
 
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
-        // Set p1 and p2 to point to the end of their respective arrays.
-        var p1 = m - 1
-        var p2 = n - 1
 
-        // And move p backwards through the array, each time writing
-        // the smallest value pointed at by p1 or p2.
-        for (p in m + n - 1 downTo 0) {
-            if (p2 < 0) {
-                break
-            }
-            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1--]
-            } else {
-                nums1[p] = nums2[p2--]
-            }
-        }
     }
 }
